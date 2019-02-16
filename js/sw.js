70e87a3ba07644373c36023765602b33065807fc
navyamarise <navya.marise@gmail.com>
2019-02-16 15:08:34 +0530

    hello

---
+++ js/sw.js
@@ -0,0 +1,24 @@
+var cache1="c2";
+this.addEventListener("install",function(event){
+	event.waitUntil(
+		caches.open(cache1).then(cache=>{
+			cache.addAll([
+				"resume.html"
+				"style.html"
+				"style.css"
+
+				])
+		})
+	);
+});
+this.addEventListener("fetch",function(event){
+ event.respondWith(
+   caches.open(cache1).then(function(cache){
+	return cache.match(event.request).then(function(res){
+		return res || fetch(event.request).then(function(reqres){
+			cache.put(event.request,reqres.clone());
+		})
+	})
+   })
+ )
+})
